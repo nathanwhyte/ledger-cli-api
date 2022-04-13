dcc5ec070af102162da11c9f56bb2fbd99d0d6c6
Nathan Whyte <nathanwhyte35@gmail.com>
2022-04-12 22:58:49 -0500

    If debug flag enabled, run function that prints

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
 use std::process::Command;
+
+#[cfg(feature = "debug_flag")]
 use std::io::{self, Write};
 
 fn main() {
@@ -8,8 +10,24 @@ fn main() {
 
 // check for new files in ledger home dir
 // add to database if new files detected
+#[cfg(not(feature = "debug_flag"))]
 fn import_ledger_data() {
+    // get the path to script (relative to project root dir)
+    let mut script_path = env::current_dir().unwrap();
+    script_path.push("src/scripts/export-ledger-to-csv.sh");
 
+    // run script
+    Command::new("sh")
+        .arg(script_path.into_os_string())
+        .output()
+        .expect("Error importing ledger data");
+}
+
+// check for new files in ledger home dir
+// add to database if new files detected
+// DEBUG VERSION: print output of script from stdout
+#[cfg(feature = "debug_flag")]
+fn import_ledger_data() {
     // get the path to script (relative to project root dir)
     let mut script_path = env::current_dir().unwrap();
     script_path.push("src/scripts/export-ledger-to-csv.sh");
@@ -20,7 +38,6 @@ fn import_ledger_data() {
         .output()
         .expect("Error importing ledger data");
 
-    #[cfg(feature = "debug")]
     println!("status: {}", output.status);
     io::stdout().write_all(&output.stdout).unwrap();
     io::stderr().write_all(&output.stderr).unwrap();
