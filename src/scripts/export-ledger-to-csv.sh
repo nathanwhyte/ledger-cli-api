b2ad541d682119f835e734289a7b431d3e75d419
Nathan Whyte <nathanwhyte35@gmail.com>
2022-04-12 23:39:33 -0500

    Fixed script to skip logged files

--- src/scripts/export-ledger-to-csv.sh
+++ src/scripts/export-ledger-to-csv.sh
@@ -6,44 +6,43 @@ PROJECT_HOME=$(pwd)
 # my default location for ledger files
 # in the future, users will be able to set their
 # default ledger directory in a config file
-LEDGER_HOME="/home/natew/Dropbox/Finance"
+LEDGER_HOME="/home/natew/Dropbox/Finance/"
 
-# go to default ledger dir. to execute commands
-cd $LEDGER_HOME
+# store the location of the log file
+LOG_FILE="$PROJECT_HOME/data/logs/processed-ledger-files.txt"
+
+# grab any .ledger or .dat files
+LEDGER_FILES=$(ls "$LEDGER_HOME" | grep ".ledger\|.dat")
 
 # convert all .ledger and .dat files to csv
-LEDGER_FILES=$(ls *.ledger *.dat)
+NEW_FILE_COUNTER=0
 for LEDGER_FILE in $LEDGER_FILES; do
 
     # remove file extension, grab base file name
     LEDGER_FILE_NAME=""
-    if [[ "${LEDGER_FILE: -4}" == ".dat" ]]; then
+    if [[ "${LEDGER_FILE#*.}" == "dat" ]]; then
         LEDGER_FILE_NAME=$(basename $LEDGER_FILE .dat)
-    else
+    elif [[ "${LEDGER_FILE#*.}" == "ledger" ]]; then
         LEDGER_FILE_NAME=$(basename $LEDGER_FILE .ledger)
+    else
+        echo "extension not supported: ${LEDGER_FILE#*.}"
     fi
 
-    # export csv data to project directory
-    ledger -f $LEDGER_FILE csv > "$PROJECT_HOME/data/ledger-csv/$LEDGER_FILE_NAME.csv"
+    if [[ -f "$LOG_FILE" ]]; then
+        if grep -Fxq "$LEDGER_FILE" "$LOG_FILE"; then
+            echo "Already processed $LEDGER_FILE"
+        else
+            printf "Adding data from $LEDGER_FILE ..."
+            ledger -f "$LEDGER_HOME/$LEDGER_FILE" csv > "$PROJECT_HOME/data/ledger-csv/$LEDGER_FILE_NAME.csv"
+            echo "$LEDGER_FILE" >> $LOG_FILE
+            ((NEW_FILE_COUNTER++))
+            printf "\rAdding data from $LEDGER_FILE ... Done\n"
+        fi
+    else
+        echo "$LEDGER_FILES" > "$LOG_FILE"
+    fi
 done
 
-LOG_FILE="$PROJECT_HOME/data/logs/processed-ledger-files.txt"
-if [[ -f "$LOG_FILE" ]]; then
-    NEW_FILE_COUNTER=0
-    for FILE in $LEDGER_FILES; do
-        if grep -Fxq "$FILE" "$LOG_FILE"; then
-            echo "Already processed $FILE"
-        else
-            echo "Adding data from $FILE ..."
-            echo "$FILE" >> $LOG_FILE
-            ((NEW_FILE_COUNTER++))
-        fi
-    done
-
-    if [[ $NEW_FILE_COUNTER != 0 ]]; then
-        echo "$NEW_FILE_COUNTER new files processed!"
-    fi
-
-else
-    echo "$LEDGER_FILES" > "$LOG_FILE"
+if [[ $NEW_FILE_COUNTER != 0 ]]; then
+    echo "$NEW_FILE_COUNTER new files processed!"
 fi
