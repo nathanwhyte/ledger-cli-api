0c00c2270ff80c4f2b8e9989a81d1669e3e76645
Nathan Whyte <nathanwhyte35@gmail.com>
2022-04-16 17:50:01 -0500

    Run `cargo fmt`

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
-mod export_ledger;
+pub mod export_ledger;
+pub mod models;
+pub mod schema;
+
+#[macro_use]
+extern crate diesel;
+extern crate dotenv;
 
 fn main() {
-    // export ledger data into csv
-    export_ledger::export_ledger_data();
+  // export ledger data into csv
+  export_ledger::export_ledger_data();
 }
