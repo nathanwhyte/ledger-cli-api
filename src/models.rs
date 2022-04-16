610afd4686124b31c5de1813baec277cac1db208
Nathan Whyte <nathanwhyte35@gmail.com>
2022-04-16 17:49:31 -0500

    Add model definitions for tables, generate schema

---
+++ src/models.rs
@@ -0,0 +1,17 @@
+#[derive(Queryable)]
+pub struct LedgerTransaction {
+  pub id: i32,
+  pub date_created: diesel::sql_types::Date,
+  pub memo: String,
+}
+
+#[derive(Queryable)]
+pub struct LedgerEntry {
+  pub id: i32,
+  pub transaction_id: i32,
+  pub bucket: String,
+  pub currency: String,
+  pub credit: bool,
+  pub amount: f32,
+  pub comment: String,
+}
---
+++ src/schema.rs
@@ -0,0 +1,23 @@
+table! {
+  ledger_entry (id) {
+    id -> Int4,
+    transaction_id -> Int4,
+    bucket -> Varchar,
+    currency -> Nullable<Varchar>,
+    credit -> Bool,
+    amount -> Float8,
+    comment -> Varchar,
+	}
+}
+
+table! {
+  ledger_transaction (id) {
+    id -> Int4,
+    date_created -> Date,
+    memo -> Varchar,
+  }
+}
+
+joinable!(ledger_entry -> ledger_transaction (transaction_id));
+
+allow_tables_to_appear_in_same_query!(ledger_entry, ledger_transaction,);
