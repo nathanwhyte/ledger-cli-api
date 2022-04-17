cbcf94e43e3dec025d898082ffb9e4e6ed815cb0
Nathan Whyte <nathanwhyte35@gmail.com>
2022-04-16 23:02:07 -0500

    Add file to handle reading + adding csv data to db

---
+++ src/parse_csv.rs
@@ -0,0 +1,37 @@
+use crate::db_utils::db_utils::establish_connection;
+
+// use super::db_utils;
+
+use csv::{ReaderBuilder, StringRecord};
+use dotenv::dotenv;
+use std::{env, fs, path::Path};
+
+pub fn handle_csv_parse() {
+    dotenv().ok();
+
+    // TODO parse csv files to get data
+    for file in fs::read_dir(Path::new(
+        &env::var("CSV_DATA_HOME").expect("error loading CSV_DATA_HOME from .env"),
+    ))
+    .unwrap()
+    {
+        // create reader for csv data from file
+        let mut csv_reader = ReaderBuilder::new()
+            .from_path(file.unwrap().path())
+            .unwrap();
+
+        // get a Vec with items being Vec with parsed csv data
+        let csv_data = csv_reader
+            .records()
+            .collect::<Result<Vec<StringRecord>, csv::Error>>()
+            .unwrap();
+
+        for entry in csv_data {
+            println!("{:?}", entry.get(0));
+        }
+
+        let _connection = establish_connection();
+
+        // TODO construct LedgerTransaction and LedgerEntry structs (use db_utils)
+    }
+}
